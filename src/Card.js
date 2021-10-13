fc1904fd976257942d8232692751a9c7421be1aa
Shaun van Huyssteen <littleblueline@gmail.com>
2021-10-13 14:23:48 +0200

    first commit

---
+++ src/Card.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class Card extends Component {
+  render() {
+    const { name, email, id } = this.props;
+    return (
+      <div className='tc bg-light-green dib br3 pa3 ma3 grow bw2 shadow-5'>
+        <img src={`https://robohash.org/${id}?200x200`} alt='Robots' />
+        <div>
+          <h2>{name}</h2>
+          <p>{email}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Card;
+
+//random comment to test git
