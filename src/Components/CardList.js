87148d7a59152a97b6477fb8cb0d3d08d5123d76
Shaun van Huyssteen <littleblueline@gmail.com>
2021-10-16 18:39:45 +0200

    added in error boundry for cardList

--- src/Components/CardList.js
+++ src/Components/CardList.js
@@ -2,6 +2,9 @@ import React from "react";
 import Card from "./Card";
 
 const CardList = ({ robots }) => {
+  if (true) {
+    throw new Error("nooooo");
+  }
   const cardComponent = robots.map((user, index) => {
     return (
       <Card
--- src/Containers/App.js
+++ src/Containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import CardList from "../Components/CardList";
 import Searchbox from "../Components/SearchBox";
 import Scroll from "../Components/Scroll";
+import ErrorBoundry from "../Components/ErrorBoundry";
 import "./App.css";
 
 class App extends Component {
@@ -41,7 +42,9 @@ class App extends Component {
           <h1>ROBOFRIENDS</h1>
           <Searchbox onSearchChange={this.onSearchChange} />
           <Scroll>
-            <CardList robots={filteredRobots} />
+            <ErrorBoundry>
+              <CardList robots={filteredRobots} />
+            </ErrorBoundry>
           </Scroll>
         </div>
       );
