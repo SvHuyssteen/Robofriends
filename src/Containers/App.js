72d0e145679c79c1418fae2828b88b429444c82e
Shaun van Huyssteen <littleblueline@gmail.com>
2021-10-16 12:28:52 +0200

    final build version

---
+++ src/Containers/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import CardList from "../Components/CardList";
+import Searchbox from "../Components/SearchBox";
+import Scroll from "../Components/Scroll";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchfield: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        return response.json();
+      })
+      .then((users) => {
+        this.setState({ robots: users });
+      });
+  }
+
+  onSearchChange = (e) => {
+    this.setState({ searchfield: e.target.value });
+  };
+
+  render() {
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    });
+
+    if (robots.length === 0) {
+      return <h1 className='tc'>Loading</h1>;
+    } else {
+      return (
+        <div className='tc'>
+          <h1>ROBOFRIENDS</h1>
+          <Searchbox onSearchChange={this.onSearchChange} />
+          <Scroll>
+            <CardList robots={filteredRobots} />
+          </Scroll>
+        </div>
+      );
+    }
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
-import CardList from "./CardList";
+import App from "./Containers/App";
 import reportWebVitals from "./reportWebVitals";
 import "tachyons";
-import { robots } from "./robots";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
